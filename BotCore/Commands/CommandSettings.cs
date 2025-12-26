0d668e53a074366b755e732f86c2db7d603b56ad
Jason Holmes <jason.j.holmes@gmail.com>
2025-12-26 19:29:55 +0900

    Clean up comments in CommandSettings.

--- BotCore/Commands/CommandSettings.cs
+++ BotCore/Commands/CommandSettings.cs
@@ -7,6 +7,6 @@ using System.Threading.Tasks;
 namespace BotCore.Commands;
 internal class CommandSettings
 {
-    // int globalCooldown { get; set; } = 3;      // Cooldown applied to all non-exempt commands
     public char commandChar { get; set; } = '!';        // Character required to precede all commands.
+    // custom cooldowns for cooldown types, etc.
 }
