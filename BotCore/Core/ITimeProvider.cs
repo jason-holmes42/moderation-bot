5d23d85afd7b3e2553457fa7e25379274168f590
Jason Holmes <jason.j.holmes@gmail.com>
2025-12-26 14:33:18 +0900

    Establish ITimeProvider and TimeProvider so that cooldowns can be properly tested.

---
+++ BotCore/Core/ITimeProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotCore.Core;
+
+// This will allow for robust testing of time-based decision-making, such as with CooldownTracker.
+internal interface ITimeProvider
+{
+    public DateTime Now { get; }
+}
